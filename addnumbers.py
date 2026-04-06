c5f098308d68d28ca967ae7c9bc6fffa18ccab96
ChelseaT-hue <thompc2652@clarkstate.edu>
2026-04-05 20:43:36 -0400

    done

--- addnumbers.py
+++ addnumbers.py
@@ -2,3 +2,18 @@
 #Watch this video or follow along in class.
 #https://www.youtube.com/watch?v=S79jbCk0K1w
 
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+""
+def printhello():
+    for i in range(10):
+        print("hello")
+
+""
+x = int(input("number 1: "))
+y = int(input("number 2: "))
+result = add_numbers(x, y)
+print("The result is: " + str(result))
+
